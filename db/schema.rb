da4436a8dfbb5f797c6b837cdb943db4bee42048
Luis Henrique Witz <luiswitz@gmail.com>
2018-09-02 22:16:29 +0100

    db schema

--- db/schema.rb
+++ db/schema.rb
@@ -10,10 +10,20 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2018_09_02_183103) do
+ActiveRecord::Schema.define(version: 2018_09_02_202509) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
+
+  create_table "order_items", force: :cascade do |t|
+    t.bigint "order_id"
+    t.bigint "product_id"
+    t.integer "quantity"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["order_id"], name: "index_order_items_on_order_id"
+    t.index ["product_id"], name: "index_order_items_on_product_id"
+  end
 
   create_table "orders", force: :cascade do |t|
     t.string "name"
@@ -41,5 +51,7 @@ ActiveRecord::Schema.define(version: 2018_09_02_183103) do
     t.datetime "updated_at", null: false
   end
 
+  add_foreign_key "order_items", "orders"
+  add_foreign_key "order_items", "products"
   add_foreign_key "orders", "shipping_modes"
 end
