fab976d3121eab4838f5520ace501c174b89eb8e
Luis Henrique Witz <luiswitz@gmail.com>
2018-09-02 20:45:51 +0100

    make order attributes required only on update

--- app/models/order.rb
+++ app/models/order.rb
@@ -1,7 +1,8 @@
 class Order < ApplicationRecord
-  belongs_to :shipping_mode
+  belongs_to :shipping_mode, optional: true
 
-  validates :name, presence: true
-  validates :email, presence: true
-  validates :address, presence: true
+  validates :name, presence: true, on: :update
+  validates :email, presence: true, on: :update
+  validates :address, presence: true, on: :update
+  validates :shipping_mode, presence: true, on: :update
 end
