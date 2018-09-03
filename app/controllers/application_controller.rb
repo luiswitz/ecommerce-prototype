f66f9a26da89e975fb612ec9368ebb1f2b66325e
Luis Henrique Witz <luiswitz@gmail.com>
2018-09-03 02:03:21 +0100

    Create current_order method

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,2 +1,15 @@
 class ApplicationController < ActionController::Base
+  helper_method :current_order
+
+  private
+
+  def current_order
+    if session[:order_id]
+      return Order.find(session[:order_id])
+    end
+
+    order = Order.create
+    session[:order_id] = order.id
+    order
+  end
 end
