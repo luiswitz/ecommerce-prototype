774b5b8e4ef1b6a023b2452f7d2568857c308e93
Luis Henrique Witz <luiswitz@gmail.com>
2018-09-04 01:08:30 +0100

    Add status column to order

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2018_09_02_202509) do
+ActiveRecord::Schema.define(version: 2018_09_03_223253) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -32,6 +32,7 @@ ActiveRecord::Schema.define(version: 2018_09_02_202509) do
     t.bigint "shipping_mode_id"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.integer "status", default: 0
     t.index ["shipping_mode_id"], name: "index_orders_on_shipping_mode_id"
   end
 
